b884f8797bd8bc26cfb5b3117b4251ac03d19d46
Raghu Betina <raghu@firstdraft.com>
2019-12-03 04:15:47 +0000

    Generate expenses resource with draft:resource

---
+++ db/migrate/20191203041547_create_expenses.rb
@@ -0,0 +1,11 @@
+class CreateExpenses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :expenses do |t|
+      t.integer :group_id
+      t.integer :category_id
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
